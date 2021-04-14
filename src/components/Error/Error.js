5c58e838b53dff079145c2d643a14fd4346f6951
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-15 02:05:19 +0600

    1st

---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 className="text-center m-5">404!!! <br/> Not Found</h1>
+        </div>
+    );
+};
+
+export default Error;
