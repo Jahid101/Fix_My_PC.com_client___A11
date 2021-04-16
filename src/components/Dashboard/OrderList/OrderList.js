50d0855d170e7b065cd8c23ce9a9bd0c1e1b207e
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-17 03:14:08 +0600

    payment added

--- src/components/Dashboard/OrderList/OrderList.js
+++ src/components/Dashboard/OrderList/OrderList.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Dashboardpage from '../Dashboardpage/Dashboardpage';
 
 const OrderList = () => {
+
+    const [order, setOrder] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:9999/orderList')
+            .then(res => res.json())
+            .then(data => setOrder(data))
+    }, [])
+
+
     return (
         <div>
             <Dashboardpage></Dashboardpage>
-            <h3 className="text-center">OrderList</h3>
+
+            <div className="mt-3" style={{ marginLeft: '300px' }}>
+                <h2 className="ml-5 mb-5">Total Booked Service : {order.length}</h2>
+                <h4 className="mb-4"><span className="ml-4 mb-5">Service Name</span> <span style={{ marginLeft: '65px' }}>Booking Time</span> <span style={{ marginLeft: '85px' }}>Order Status</span> </h4>
+                {
+                    order.map(order =>
+                        <div className="d-flex rounded shadow-lg mb-3 w-75">
+                            <h6 className="m-5">{order.service}</h6>
+                            <h6 className="m-5">{order.orderTime}</h6>
+                            <button className="btn btn-primary m-5">Pending...</button>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/components/Dashboard/ServiceList/ServiceList.js
+++ src/components/Dashboard/ServiceList/ServiceList.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../App';
 import Dashboardpage from '../Dashboardpage/Dashboardpage';
 
 const ServiceList = () => {
+
+    const [service, setService] = useState([]);
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    useEffect(() => {
+        fetch('http://localhost:9999/serviceList?email=' + loggedInUser.email)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [loggedInUser.email])
+
+
     return (
-        <div className="">
+        <div>
             <Dashboardpage></Dashboardpage>
-            <h2 className="text-center">ok</h2>
+
+            <div className="mt-3" style={{ marginLeft: '300px' }}>
+                <h2 className="ml-5 mb-5">Total Booked Service : {service.length}</h2>
+                <h4 className="mb-4 form-control text-light bg-info w-75">
+                    <strong>
+                        <span className="ml-4 mb-5">Service Name</span>
+                        <span style={{ marginLeft: '85px' }}>Booking Time</span>
+                        <span style={{ marginLeft: '180px' }}>Order Status</span>
+                    </strong>
+                </h4>
+                {
+                    service.map(service =>
+                        <div className="d-flex rounded shadow-lg mb-3 w-75">
+                            <h6 className="m-5">{service.service}</h6>
+                            <h6 className="m-5">{service.orderTime}</h6>
+                            <button className="btn btn-primary m-5">Pending...</button>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
