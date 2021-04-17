fc74f834f3a596fbdaa5fb184ab47842bbdbceb7
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-18 05:05:16 +0600

    some changes

--- src/components/Home/TestimonialCard/TestimonialCard.js
+++ src/components/Home/TestimonialCard/TestimonialCard.js
@@ -6,8 +6,8 @@ const TestimonialCard = (props) => {
     const {name, email, description} = props.testimonial;
 
     return (
-        <div className="cardDesign col">
-            <div className="container testimonialCardInfo mt-5 p-3 btn">
+        <div className="col">
+            <div className="container cardDesign testimonialCardInfo mt-5 p-3 btn">
                 <div>
                     <h3 className="text-center mt-4">{name}</h3>
                     <h6 className="mt-4">{email}</h6>
